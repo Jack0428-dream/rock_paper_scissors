600eb54e016e5d6330898a69f175a24b03d45c31
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-04-07 11:58:14 +0900

    get computer choice

---
+++ rpscript.js
@@ -0,0 +1,17 @@
+// console.log("helloworld");
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 100) + 1;
+
+    if (randomChoice >= 1 || randomChoice < 33) {
+        return "rock";
+    } else if (randomChoice >=33 || randomChoice < 66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+getComputerChoice();
+
+console.log(randomChoice);
