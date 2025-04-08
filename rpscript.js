ae37225757ec63abec1f44a62136204e396b5478
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-04-08 13:44:25 +0900

    change the variables for decalring

--- rpscript.js
+++ rpscript.js
@@ -8,18 +8,47 @@ function getComputerChoice() {
     } else if (randomChoice >=33 && randomChoice < 66) {
         randomChoice = "paper";
     } else {
-        randomChoice = "Scissors";
+        randomChoice = "scissors";
     }
-//    console.log(randomChoice);
+    return randomChoice;
 }
 
 function getHumanChoice() {
-    let userChoice = prompt("Please enter your choice among rock, paper, scissors");
-//    console.log(userChoice);
+    let userinput = prompt("Please enter your choice among rock, paper, scissors");
+    let userChoice = userinput.toLowerCase();
+    return userChoice;
 }
 
-getComputerChoice();
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice == "rock" && computerChoice == "paper" ) {
+        console.log("You lose! Paper beats Rock");
+    } else if(humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock crushes Scissors");
+    } else if(humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock");
+    } else if(humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors tear up Paper")
+    } else if(humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors tear up Paper");
+    } else if(humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock is too solid for your Scissors");
+    } else {
+        console.log("something went wrong check your inputs")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+let humanScore = 0 ;
+let computerScore = 0; 
+
+// console.log(humanSelection);
+// console.log(computerSelection);
+
+playRound(humanSelection,computerSelection);
+
 
 
 
